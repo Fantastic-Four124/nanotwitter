dfb196bf612376d2bdeed06ce9429963b2bccd12
vagrant <vagrant@precise64.(none)>
2018-02-16 21:29:56 +0000

    Renamed 'spec' folder to 'test,' added 'login.erb' and added basic, INCOMPLETE authentication

--- service.rb
+++ service.rb
@@ -2,4 +2,31 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'byebug'
 
+set :bind, '0.0.0.0' # Needed to work with Vagrant
 
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    redirect '/login', 'Incorrect username or password'
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    # In addition to the checks below, does a call to the user database with the given username and password even exist?
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == #Insert existing username and password combination
+  end
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+
+end
+
+# All other pages should have "protected!" as the first thing that they do.
+get '/' do
+  protected!
+end
