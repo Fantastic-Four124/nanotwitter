5610277e5e26ceb64ad68f3b53d6cd77a87b8105
Zhengyang Zhou <zhengyjo@brandeis.edu>
2018-02-27 15:09:20 -0500

    update follow

--- models/user.rb
+++ models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   validates :digest,presence:true
   validates_uniqueness_of :username
   has_many :users, through: :follows
+  has_many :followings, :class_name => "Follow",:foreign_key => :followed_user_id
+  has_many :followers, :through => :followings,:source => :user
   has_many :tweets
   has_many :tweets, through: :mentions
 
