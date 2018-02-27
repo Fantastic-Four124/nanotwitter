e7937dd3958c86fc1157e37addfad326a3a45121
PostgreSQL administrator <postgres@precise64.(none)>
2018-02-27 23:48:36 +0000

    finished basic authentication

--- service.rb
+++ service.rb
@@ -6,67 +6,66 @@ enable :sessions
 
 set :bind, '0.0.0.0' # Needed to work with Vagrant
 
-configure do
-  set :twitter_client, false
-end
+# configure do
+#   set :twitter_client, false
+# end
 
+# Small helper that minimizes code
 helpers do
   def protected!
-    return settings.twitter_client # for testing only
-    return if authorized?
-    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
-    redirect '/login', 'Incorrect username or password'
-  end
-
-  def authorized?
-    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    # In addition to the checks below, does a call to the user database with the given username and password even exist?
-    #@auth.provided? and @auth.basic? and @auth.credentials and User.exist?(name: @auth.credentials[0], password: #@auth.credentials[1])
+    #return settings.twitter_client # for testing only
+    return !session[:username].nil?
   end
 end
 
 get '/login' do
+  #byebug
   erb :login
 end
 
 post '/login' do
-  username = params['username']
-  password = params['password']
-  if username == '105' && password == 'pw'
-    settings.twitter_client = true
+  if params['username'] == '105' && params['password'] == 'pw' # User.exist?(username: session[:username], password: session[:password])
+    session[:username] = params['username']
+    session[:password] = params['password']
     redirect '/'
   else
+
     @texts = 'Wrong password or username.'
     erb :login
   end
 end
 
-# All other pages should have "protected!" as the first thing that they do.
+# All other pages need to have these session objects checked.
 get '/' do
+  #byebug
   if protected!
-    @texts = 'logined'
+    @texts = 'logged in'
     erb :home
   else
     erb :login
   end
 end
 # All other pages should have "protected!" as the first thing that they do.
-get '/user/register'
+get '/user/register' do
   if protected!
-    @texts = 'logined'
+    @texts = 'logged in'
     erb :home
   else
     erb :register
   end
 end
 
-get '/search'
+get '/search' do
   if protected!
-    @texts = 'logined'
+    @texts = 'logged in'
     erb :search
   else
     redirect '/login'
   end
 end
 
-get ''
+post '/logout' do
+  session.delete(:username)
+  session.delete(:password)
+  redirect '/'
+end
