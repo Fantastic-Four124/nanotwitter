ca51c414767c613023aaa8ff6e8f517f599b5385
Alex Hu <alexhubnds@icloud.com>
2018-03-02 13:34:10 -0500

    tests

--- test/service_test.rb
+++ test/service_test.rb
@@ -2,5 +2,28 @@ require 'minitest/autorun'
 require "./service.rb"
 
 class REPLTest < Minitest::Test
+  def test_login
+
+  end
+
+  def test_login_home
+
+  end
+
+  def test_login_wrong_person
+
+  end
   
+  def test_unlogin_home
+
+  end
+
+  def test_signup 
+
+  end
+
+  def test_logout
+
+  end
+
 end
