d65cb5b5c9998d0afa28046dbad11fa148a7cf48
Tom Willkens <tom.willkens@gmail.com>
2018-03-03 20:38:24 -0500

    minor user search bugfix

--- service.rb
+++ service.rb
@@ -147,8 +147,7 @@ get '/search' do
     @no_term = false
     if /([@.])\w+/.match(term)
       term = term[1..-1]
-      @results = User.where("username like ?", "%#{term}%").sort_by &:created_at
-      @results.reverse!
+      @results = User.where("username like ?", "%#{term}%")
       @user_search = true
     else
       @results = Tweet.where("message like ?", "%#{term}%").sort_by &:created_at
