f56ae5fde8e20a096da1aed3b1f271375fb018f1
Alex Hu <alexhubnds@icloud.com>
2018-03-06 15:26:09 -0500

    setting up

--- test_interface.rb
+++ test_interface.rb
@@ -17,18 +17,72 @@ post '/test/reset/all' do
 end
 
 
-post "/test/reset/testuser" do
-  
+post '/test/reset/testuser' do
+  # TODO: DO SOMETHING
+  # TODO: DO SOMETHING
+  # TODO: DO SOMETHING
+  puts params
 end
 
+# Report the current version
 get '/test/version' do
-  Version.VERSION.to_json
+  "Version: #{Version.VERSION}".to_json
 end
 
-get '/test/status' do 
-
+# One page report
+# How many users, follows, and tweets are there. What is the TestUser’s id
+get '/test/status' do
+  # TODO: DO SOMETHING
+  # TODO: DO SOMETHING
+  # TODO: DO SOMETHING
+  puts params
+  'STATUS'.to_json
 end
 
-post '/test/reset/standard?tweets=' do 
-
+# Read from seed
+# correct format should be /test/reset/standard?tweets=6
+post '/test/reset/standard?' do
+  puts params
+  input = params[:tweets]
+  begin
+    if params.length == 1
+      num = -1 # -1 means no limit
+    else
+      num = Integer(input) # only load n tweets from seed data
+    end
+    # TODO: DO SOMETHING
+    # TODO: DO SOMETHING
+    # TODO: DO SOMETHING
+    "GOOD: #{num}".to_json
+  rescue
+    # Wrong input
+    "Nah, input is not valid, \nparams = #{params}".to_json
+  end
 end
+
+# create u (integer) fake Users using faker. Defaults to 1.
+# each of those users gets c (integer) fake tweets. Defaults to zero.
+# Example: /test/users/create?count=100&tweets=5
+post '/test/users/create?' do
+  input_count = params[:count]
+  input_tweet = params[:tweets]
+  begin
+    count = Integer(input_count)
+    tweet = Integer(input_tweet)
+    # TODO: DO SOMETHING
+    # TODO: DO SOMETHING
+    # TODO: DO SOMETHING
+    "GOOD:\n\tcount: #{count}, tweet: #{tweet}".to_json
+  rescue
+    # Wrong input
+    "Nah, input is not valid, \nparams = #{params}".to_json
+  end
+end
+
+# ??? User u is a number?
+post '/test/user' do
+  puts params
+  'TODO'.to_json
+end
+
+
